aa6ab9f725031674aa4401d138233e724664b4e5
dohanjun <dohanjun@email.com>
2024-11-12 14:46:59 +0900

    2024-11-12

--- JS/forexe1.js
+++ JS/forexe1.js
@@ -15,16 +15,19 @@
 // document.write('</table>');
 
 
-const num = 3;
-document.write('<table border="2"')
-for (let i = 1; i <= 9; i++) {
-  if(i % 2 == 0){
-    document.write('<tr style="background: red; color: #fff;"><td>' + num + ' * ' + i +'</td><td> = </td><td>' + num * i + '</td></tr>')  
-  } 
-  else{
-    document.write('<tr style="background: blue; color: #fff;"><td>' + num + ' * ' + i +'</td><td> = </td><td>' + num * i + '</td></tr>') 
-  }
+document.write('<table border="2"');
+for (let j=2; j<=9; j++){
+  const num = j; // 2~9까지 순차적으로 변환
+  for (let i = 1; i <= 9; i++) {
+    if(i % 2 == 0){
+      document.write(`<tr style="background: red; color: #fff;"><td>${num} * ${i}</td><td> = </td><td> ${num * i}</td></tr>`)  
+    } 
+    else{
+      document.write(`<tr style="background: blue; color: #fff;"><td>${num} * ${i}</td><td> = </td><td> ${num * i}</td></tr>`) 
+    }
+  }// end of for(let i=1...)
 } 
+document.write('</table>');
 
 
 
--- JS/forexe3.js
+++ JS/forexe3.js
@@ -12,19 +12,16 @@
 let max = 0;
 let average = 0;
 let sum = 0;
-
-
-
-
-
-
-
+let min = 100;
 for (let i=1; i <= 5; i++) {
   let score = prompt('점수를 입력');
   sum = sum + parseInt(score);
  if(max < score) {
   max = score;
+ }
+ if(min > score){
+  min = score;
  }   
 }
 average = sum / 5;
-document.write(`합계는${sum}이고 평균은${average}이고 최고점수는${max}입니다.`);
+document.write(`합계는${sum}이고 평균은${average}이고 최고점수는${max}이고 최저점수는${min}입니다.`);
