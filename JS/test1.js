7d679a208fe06ddf63a4157a04a9a96ec377a5c4
dohanjun <dohanjun@email.com>
2024-11-13 09:25:10 +0900

    test

---
+++ JS/test1.js
@@ -0,0 +1,32 @@
+// test.js
+//1. 1~10 까지의 숫자의 합.
+//1~10의 합 : 55입니다
+
+document.querySelector('#inputValue').remove(); // 요소(element) 삭제
+document.querySelector('#addBtn').remove();  // 요소(element) 삭제
+document.querySelector('span').remove(); // 요소(element) 삭제
+
+// let sum = 0;
+
+// for (let i = 1; i <= 10; i++) {
+  
+//   document.write(`1~10까지의 숫자의 합은 ${sum}입니다.`)
+// }
+
+//   const num = 4;
+
+// for (let i= 1; i <= 9; i++) {
+//   document.write(`${num} * ${i} = ${num * i}<br>`)
+// };
+
+
+let numAry = [23, 66, 92, 71, 52, 49];
+let sum = 0;
+
+for (let i = 0; i < numAry.length; i++) {
+  
+  if (numAry[i] % 2 == 1){
+    sum += numAry[i];
+  };
+}
+document.write(`홀수의 합은 ${sum}입니다.`)
