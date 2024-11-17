ad9c413890db1bd074ee2e52d70180952cadf3a8
dohanjun <ehgkswns132@naver.com>
2024-11-17 23:09:56 +0900

    2024-11-15homework

--- JS/lotto.exe.js
+++ JS/lotto.exe.js
@@ -1,38 +1,22 @@
- // 임의의 숫자를 6개를 생성함 범위는 1~45까지
-
+// 임의의 숫자를 6개를 생성함 범위는 1~45까지
 
 function lottoNumber() {
-  let lottoAry = [];
-  for (let i = 0; i < 6){
-    let temp = parseInt(Math.random() * 45 ) + 1
+  let lottoAry = [temp];
+  for (let i = 0; i < lottoAry ) {
+    let temp = parseInt(Math.random() * 45) + 1;
   }
-};
-
-
-
-
-
-
-
-
-
-
-
+  for (let j = 1; j < 6; j++){
+    lottoAry.push(temp)
+  }
+}
 
 //버튼이벤트
 
-document.querySelector('#btn').onclick = function() {
-  
-}
-
-
-
- 
-
-
-
-
+document.querySelector("#btn").onclick = function () {
+  document.querySelector("#lotto").innerHTML =
+    document.querySelector("#lotto").innerHTML + `<span>${lottoAry[i]}<span>` = numary;
+};
 
 // parseInt(Math.random('span') * 45 ) + 1
 
- 
+// span 안에 넣을 값만
